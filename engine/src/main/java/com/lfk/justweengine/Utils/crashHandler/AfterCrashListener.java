4f9880415d66c0097639731308b4643bf40b5381
JustWe <lfk_dsk@hotmail.com>
2016-02-07 23:10:30 +0800

    update Info

--- engine/src/main/java/com/lfk/justweengine/Utils/crashHandler/AfterCrashListener.java
+++ engine/src/main/java/com/lfk/justweengine/Utils/crashHandler/AfterCrashListener.java
@@ -1,7 +1,11 @@
 package com.lfk.justweengine.Utils.crashHandler;
 
 /**
- * Created by liufengkai on 16/1/17.
+ * AfterCrashListener 
+ * save info after crash
+ *
+ * @author liufengkai
+ *         Created by liufengkai on 16/1/17.
  */
 public interface AfterCrashListener {
     void AfterCrash();
