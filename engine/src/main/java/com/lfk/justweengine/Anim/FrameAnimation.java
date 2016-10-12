26a6e0533084f04eaadfbc445f366003f50e6a92
hnshilei <hnshilei@163.com>
2016-10-12 14:14:05 +0800

    Update FrameAnimation.java (#10)
    重载了构造函数，添加了interval选项，可以实现动画切换间隔时间控制，单位ms.

--- engine/src/main/java/com/lfk/justweengine/Anim/FrameAnimation.java
+++ engine/src/main/java/com/lfk/justweengine/Anim/FrameAnimation.java
@@ -10,6 +10,8 @@ public class FrameAnimation extends BaseAnim {
     private int firstFrame;
     private int lastFrame;
     private int direction;
+    private int interval;//帧动画切换时间间隔，单位ms
+    private GameTimer timer;
 
     public FrameAnimation(int firstFrame, int lastFrame, int direction) {
         this.firstFrame = firstFrame;
@@ -17,15 +19,30 @@ public class FrameAnimation extends BaseAnim {
         this.direction = direction;
         this.animType = AnimType.FRAME;
         this.animating = true;
+        this.interval = 0;
+        timer = new GameTimer();
+    }
+    
+    public FrameAnimation(int firstFrame, int lastFrame, int direction, int interval) {
+        this.firstFrame = firstFrame;
+        this.lastFrame = lastFrame;
+        this.direction = direction;
+        this.animType = AnimType.FRAME;
+        this.animating = true;
+        this.interval = interval;
+        timer = new GameTimer();
     }
 
     @Override
     public int adjustFrame(int ori) {
-        int modified = ori + direction;
-        if (modified < firstFrame) {
-            modified = lastFrame;
-        } else if (modified > lastFrame) {
-            modified = firstFrame;
+        int modified = ori;
+        if (timer.stopWatch(interval)) {
+            modified += direction;
+            if (modified < firstFrame) {
+                modified = lastFrame;
+            } else if (modified > lastFrame) {
+                modified = firstFrame;
+            }
         }
         return modified;
     }
